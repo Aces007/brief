104cb4e8f64a1dc76268bbc275ffad573112e99f
Istong Lang Malakas <clavanoace@gmail.com>
2021-11-09 17:37:38 -0800

    edited

--- Money=nApplesRefined.py
+++ Money=nApplesRefined.py
@@ -1,7 +1,8 @@
 def Survey():
     PeraNaMeronkaNgayon = int(input("How much money do you currently have?:"))
     MagkanoBaYungMansanas = int(input("How much does the apple cost?:"))
-    return PeraNaMeronkaNgayon, MagkanoBaYungMansanas
+    MagkanoBudgetMo = int(input("How much is your budget for the fruit?: "))
+    return PeraNaMeronkaNgayon, MagkanoBaYungMansanas, MagkanoBudgetMo
 
 # Like the 2nd program this function contains all the calculations necessary to present the apples you can 
 # buy and change to receive.
@@ -11,12 +12,14 @@ def calculations():
     return ApplesNaMabibiliMo, MagkanoMatitiraSaPeraMo
 
 
-def MagkanoLahatNgNagastosKo():
-    print(f"You can buy {MansanasYouAcquired} apples and your change is {SukliMoAy}")
+def BudgetCalc():
+    if MagkanoBudget < PeraNaMeronKa:
+        print(f"You can buy {MansanasYouAcquired} apples and your change is {SukliMoAy}")
+    
 
 # So, the last program I did these user inputs are separated, now for a more simpler presentation of the progam these were mixed together.
 # 1st Step: These two will ask for your current money, and the prices given for the apples you're willing to buy.
-PeraNaMeronKa, MagkanoApples = Survey()
+PeraNaMeronKa, MagkanoApples, MagkanoBudget = Survey()
 
 # 2ndStep: Then these will perform the calculations of the number of apples that you can buy with your money and 
 # as a result the prorgam will present you the amount of change you will get.
@@ -24,4 +27,4 @@ MansanasYouAcquired, SukliMoAy = calculations()
 
 # Last Step: Lastly I decided to separate the print function from the calculations and user inputs to not cause confusion.
 # This will present you the apples you can buy with the money you currently have and the change you will receive from the expenses.
-MagkanoLahatNgNagastosKo()
+BudgetCalc()
